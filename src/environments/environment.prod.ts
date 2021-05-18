88fb8e7c6e174c4d86f83873d0638a96462c32c5
Byinfazci <84033400+Byinfazci@users.noreply.github.com>
2021-05-19 00:18:50 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   endpoint: '/api/v1',
   rootEndpoint: 'https://lexon.herokuapp.com/',
-  discordInvite: 'https://discord.gg/uDTgxyg',
+  discordInvite: 'https://discord.gg/Acm8eUyXaK',
   docsURL: 'https://lexon.herokuapp.com/',
   githubURL: 'https://github.com/Byinfazci/Website',
   version: 'v1.0.3b',
